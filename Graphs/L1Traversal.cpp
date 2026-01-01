ec818091c287387f0d35198547236e9ecad4e2a0
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-01 23:02:53 +0530

    refactor: added super imp loop in bfs to handle disconnected components

--- Graphs/L1Traversal.cpp
+++ Graphs/L1Traversal.cpp
@@ -1,100 +1,120 @@
-#include<iostream>
-#include<queue>
-#include<unordered_map>
-#include<map>
-#include<vector>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <vector>
 using namespace std;
 
+class Graph
+{
+public:
+    // adjacency list
+    unordered_map<int, vector<int>> adjList;
+    //   map<int , vector<int> > adjList;
 
- class Graph{
-  public:
-  //adjacency list
-  unordered_map<int , vector<int> > adjList;
-//   map<int , vector<int> > adjList;
-
-  //direction = 1 -> directed edge
-  //direction = 0 -- undirected edge
-  void addEdge(int u, int v, bool direction){
-    if(direction == 1){
-        //directed edge -> u->v
-        adjList[u].push_back(v);
-    }
-    else{
-        //undirected edge
-        //u--v equivalent to  u->v , v->u
-        adjList[u].push_back(v);
-        adjList[v].push_back(u);
-    }
-  }
-
-  void printAdjacencyList(){
-    for(auto i : adjList){
-        // i -> record or pair of key and value
-        // i.first -> int
-        // i.second -> vector<int>
-        cout << i.first << ": ";
-        cout << "{";
-        for(auto j: i.second){
-            cout << j <<", ";
+    // direction = 1 -> directed edge
+    // direction = 0 -- undirected edge
+    void addEdge(int u, int v, bool direction)
+    {
+        if (direction == 1)
+        {
+            // directed edge -> u->v
+            adjList[u].push_back(v);
         }
-        cout << "}" << endl;
+        else
+        {
+            // undirected edge
+            // u--v equivalent to  u->v , v->u
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
     }
-  }
 
-  void bfsTraversal(int src){
-    queue<int> q;
-    unordered_map<int,bool> visited;
-    //initial state
-    q.push(src);
-    visited[src] = true;
-    //main logic
-    while(!q.empty()){
-         int frontNode = q.front();
-         q.pop();
-         cout << frontNode << ", ";
-         for(auto nbr: adjList[frontNode]){
-            // hr ek nbr ko queue me insert krna hai but
-            //insert tabhi karna hai, agar unvisited ho
-            if(!visited[nbr]){
-                q.push(nbr);
-                visited[nbr] = true;
+    void printAdjacencyList()
+    {
+        for (auto i : adjList)
+        {
+            // i -> record or pair of key and value
+            // i.first -> int
+            // i.second -> vector<int>
+            cout << i.first << ": ";
+            cout << "{";
+            for (auto j : i.second)
+            {
+                cout << j << ", ";
             }
-         }
-    }
-
-  }
-  //entire DFS logic resides inside the dfsHelper function
-  void dfshelper(int src, unordered_map<int, bool> &visited){
-    //jaise hi call aai , toh sbse pehle ans print kro
-    //visited true mark karo
-    cout << src << ", ";
-    visited[src] = true;
-    //ab current node se iske nbr ki taraf call marni hai
-    for(auto nbr: adjList[src]){
-        //agar nbr visited nahi hai
-        //toh uske liye DFS call karna hai
-        if(!visited[nbr]){
-            dfshelper(nbr, visited);
+            cout << "}" << endl;
         }
     }
-  }
-  void dfsTraversal(int src , int n){
-    unordered_map<int,bool> visited;
-    //fn call for src node
-    //SUPER IMP LOOP..handles disconnected components 
-    for(int src=0; src< n; src++){
-        if(!visited[src]){
-             dfshelper(src, visited);
+
+    void bfsTraversal(int n)
+    {
+        queue<int> q;
+        unordered_map<int, bool> visited;
+
+        // SUPER IMPORTANT LOOP (same idea as DFS)
+        for (int src = 0; src < n; src++)
+        {
+            if (!visited[src])
+            {
+                q.push(src);
+                visited[src] = true;
+
+                while (!q.empty())
+                {
+                    int frontNode = q.front();
+                    q.pop();
+                    cout << frontNode << ", ";
+
+                    for (auto nbr : adjList[frontNode])
+                    {
+                        if (!visited[nbr])
+                        {
+                            q.push(nbr);
+                            visited[nbr] = true;
+                        }
+                    }
+                }
+            }
         }
     }
-    
-  }
 
+    // entire DFS logic resides inside the dfsHelper function
+    void dfshelper(int src, unordered_map<int, bool> &visited)
+    {
+        // jaise hi call aai , toh sbse pehle ans print kro
+        // visited true mark karo
+        cout << src << ", ";
+        visited[src] = true;
+        // ab current node se iske nbr ki taraf call marni hai
+        for (auto nbr : adjList[src])
+        {
+            // agar nbr visited nahi hai
+            // toh uske liye DFS call karna hai
+            if (!visited[nbr])
+            {
+                dfshelper(nbr, visited);
+            }
+        }
+    }
+    void dfsTraversal(int src, int n)
+    {
+        unordered_map<int, bool> visited;
+        // fn call for src node
+        // SUPER IMP LOOP..handles disconnected components
+        for (int src = 0; src < n; src++)
+        {
+            if (!visited[src])
+            {
+                dfshelper(src, visited);
+            }
+        }
+    }
 };
 
-
-int main(){
-    Graph g;    
+int main()
+{
+    Graph g;
 
     g.addEdge(0, 1, 1);
     g.addEdge(0, 2, 1);
@@ -106,11 +126,11 @@ int main(){
     g.addEdge(4, 7, 1);
     g.addEdge(5, 6, 1);
     g.addEdge(7, 6, 1);
-    
+
     g.printAdjacencyList();
-    // g.bfsTraversal(0);
-    cout << "DFS: ";
-    g.dfsTraversal(0, 9);
+    g.bfsTraversal(9); // 9 is no. of nodes ..no use of src in bfs
+    // cout << "DFS: ";
+    // g.dfsTraversal(0, 9);
 
     return 0;
 }
