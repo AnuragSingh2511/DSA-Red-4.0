7c859e7bdf19279e8ea65ddfcc81ce21505c7310
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-01 21:15:13 +0530

    feat: graph implementation adjacency list

--- Graphs/Lecture1.cpp
+++ Graphs/Lecture1.cpp
@@ -1,13 +1,56 @@
 #include<iostream>
+#include<queue>
+#include<unordered_map>
+#include<vector>
 using namespace std;
 
 class Graph{
+  public:
+  //adjacency list
+  unordered_map<int , vector<int> > adjList;
+
+  //direction = 1 -> directed edge
+  //direction = 0 -- undirected edge
+  void addEdge(int u, int v, bool direction){
+    if(direction == 1){
+        //directed edge -> u->v
+        adjList[u].push_back(v);
+    }
+    else{
+        //undirected edge
+        //u--v equivalent to  u->v , v->u
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+  }
+
+  void printAdjacencyList(int n){
+    for(int i = 0; i<n ; i++){
+        // i -> record or pair of key and value
+        // i.first -> int
+        // i.second -> vector<int>
+        cout << i << ": ";
+        cout << "{";
+        for(auto j: adjList[i]){
+            cout << j <<", ";
+        }
+        cout << "}" << endl;
+    }
+  }
+
 
 };
 
 
 int main(){
-
+    Graph g;
+    
+    g.addEdge(0, 1, 1);
+    g.addEdge(1, 2, 1);
+    g.addEdge(2, 3, 1);
+    g.addEdge(3, 4, 1);
+    //graph looks like 0->1->2->3->4
+    g.printAdjacencyList(5);
 
     return 0;
 }
