d8b11e284cf8ee9d30b1a8e75372b7cf32322782
Anurag Singh <anurocks.aps@gmail.com>
2026-02-06 20:51:55 +0530

    feat: length of singly linked list

---
+++ Linked-Lists/L1length.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+     int data;
+     Node* next;
+
+     //Constructor
+     Node(int value){
+        this->data = value;
+        this->next = NULL;
+     }
+};
+
+void insertAtHead(int value, Node* &head, Node* &tail){
+  
+  if(head == NULL && tail == NULL){
+    //empty ll
+     Node* newNode = new Node(value);
+     head = newNode;
+     tail = newNode;
+  }
+  
+  else{
+     //non-empty ll
+     Node* newNode = new Node(value);
+     newNode->next = head;
+     head = newNode;
+  }
+}
+
+void insertAtTail(int value, Node* &head, Node* &tail){
+    if(head == NULL && tail == NULL){
+    //empty ll
+     Node* newNode = new Node(value);
+     head = newNode;
+     tail = newNode;
+  }
+  
+  else{
+     //non-empty ll
+     Node* newNode = new Node(value);
+     tail->next = newNode;
+     tail = newNode;
+  }
+}
+
+void printLinkedList(Node* head){
+  Node* temp = head;
+
+  while(temp != NULL){
+    cout << temp->data << "->";
+    temp = temp->next;
+  }
+  cout << endl;
+}
+
+int lengthLinkedList(Node* head){
+    Node* temp = head;
+    int len = 0;
+
+    while(temp != NULL){
+        len++;
+        temp = temp -> next;
+    }
+    return len;
+}
+
+
+int main(){
+   
+    //Empty linked-list
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtHead(10, head, tail);
+    insertAtHead(20, head, tail);
+    insertAtHead(30, head, tail);
+    insertAtTail(40, head, tail);
+    printLinkedList(head);
+    
+    int ans = lengthLinkedList(head);
+    cout << ans;
+
+    return 0;
+}
