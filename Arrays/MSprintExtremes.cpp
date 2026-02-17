36cd36a554368095235c1cf6004ebf361ca92204
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-02-17 13:49:01 +0530

    chore: t.c and s.c comment

--- Arrays/MSprintExtremes.cpp
+++ Arrays/MSprintExtremes.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//O(nlogn) , O(n)
 vector<int>printExtremeElementsAlternately(vector<int>& arr) { 
         vector<int> ans;
         sort(arr.begin(), arr.end());   
