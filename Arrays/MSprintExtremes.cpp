40a3b65c761e8246a2e349d5b0a80011f867e914
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-02-17 22:18:44 +0530

    feat: lakshay solution print Extremes

--- Arrays/MSprintExtremes.cpp
+++ Arrays/MSprintExtremes.cpp
@@ -1,17 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//O(nlogn) , O(n)
+//O(nlogn) , O(n).. my solution
+// vector<int>printExtremeElementsAlternately(vector<int>& arr) { 
+//         vector<int> ans;
+//         sort(arr.begin(), arr.end());   
+
+//         int i = 0; int j = arr.size() - 1;
+//         while(i <= j){
+//             if(i == j) ans.push_back(arr[i++]);
+//             else{
+//             ans.push_back(arr[i++]);
+//             ans.push_back(arr[j--]);
+//             }            
+//         }
+//         return ans;
+// }
+
+//lakshay solution
 vector<int>printExtremeElementsAlternately(vector<int>& arr) { 
         vector<int> ans;
         sort(arr.begin(), arr.end());   
 
         int i = 0; int j = arr.size() - 1;
+        bool amIPickingSmallElement = true;
+
         while(i <= j){
-            if(i == j) ans.push_back(arr[i++]);
+            if(amIPickingSmallElement) {
+                ans.push_back(arr[i++]);
+                amIPickingSmallElement = false;
+            }
             else{
-            ans.push_back(arr[i++]);
             ans.push_back(arr[j--]);
+            amIPickingSmallElement = true;
             }            
         }
         return ans;
